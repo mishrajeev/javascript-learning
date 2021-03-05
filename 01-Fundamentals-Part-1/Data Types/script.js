1beb2989131001d4a6766aadd858a777e94e7284
Rajeev Mishra <20rajeevmishra@gmail.com>
2021-03-06 00:00:54 +0100

    data types

---
+++ 01-Fundamentals-Part-1/Data Types/script.js
@@ -0,0 +1,42 @@
+/* let js = 'Amazing';
+if(js === 'Amazing'){
+    //alert('JS is fun');
+}
+console.log(40 + 8 + 23 );
+let firstName = "Rajeev";
+let lastName = "Mishra";
+let PI = 3.1514;
+console.log(firstName + " " + lastName); */
+console.log("----------------------------------------");
+
+console.log(true);
+let javascriptIsFun = true;
+// console.log("JS is fun " + javascriptIsFun);
+console.log(typeof javascriptIsFun);
+
+// console.log(typeof "javascriptIsFun");
+
+// console.log(typeof 12.0);
+
+javascriptIsFun = 'Yo';
+console.log(javascriptIsFun);
+console.log(typeof "javascriptIsFun");
+console.log("----------------------------------------");
+
+let year;
+
+console.log(year);
+console.log(typeof year);
+console.log("----------------------------------------");
+
+year = 2021;
+console.log(year);
+console.log(typeof year);
+
+console.log("----------------------------------------");
+
+console.log(typeof null);
+
+console.log("----------------------------------------");
+
+console.log("----------------------------------------");
