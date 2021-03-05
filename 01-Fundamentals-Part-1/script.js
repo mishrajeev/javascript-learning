6c8d852a3ea3cda194172942961c8f4362edde41
Rajeev Mishra <20rajeevmishra@gmail.com>
2021-03-05 23:28:41 +0100

    variables

--- 01-Fundamentals-Part-1/script.js
+++ 01-Fundamentals-Part-1/script.js
@@ -1,5 +1,9 @@
 let js = 'Amazing';
 if(js === 'Amazing'){
-    alert('JS is fun');
+    //alert('JS is fun');
 }
 console.log(40 + 8 + 23 );
+
+let firstName = "Rajeev";
+let lastName = "Mishra";
+console.log(firstName + " " + lastName);
