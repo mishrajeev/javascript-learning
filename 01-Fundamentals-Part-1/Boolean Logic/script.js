ac31cd77e92bce26f5e47c266787fe5dec143138
Rajeev Mishra <20rajeevmishra@gmail.com>
2021-03-08 23:51:24 +0100

    Boolean logic and operators

---
+++ 01-Fundamentals-Part-1/Boolean Logic/script.js
@@ -0,0 +1,11 @@
+// Boolean Logic : AND, OR, NOT Operators
+
+
+
+const a = true;
+const b = false;
+
+console.log(!a); // NOT a
+console.log(!b); // NOT b
+console.log( a && b ); //  a AND b
+console.log( a || b ); // a OR b
