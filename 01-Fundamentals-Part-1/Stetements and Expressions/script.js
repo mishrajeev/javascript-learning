51c35d7464f82f343925b8e0e8ec26c8b06767f7
mish.ra.jeev <20rajeevmishra@gmail.com>
2021-03-09 10:19:46 +0100

    Statements and Expressions

---
+++ 01-Fundamentals-Part-1/Stetements and Expressions/script.js
@@ -0,0 +1,25 @@
+ 
+//Expression
+1
+1+1
+1234
+true && false && !true
+"Rajeev"
+
+ 
+//Statement
+if(true){
+
+}
+
+console.log("Rajeev"); // Statement
+const name1 = "Rajeev Mishra"; // Statement
+
+// In template literal we insert ONLY Expression
+
+console.log(`I'm ${name1}`);
+console.log(`I'm ${2021 - 1980} years old` );
+
+console.log(`I can drive car: ${(2021 - 1980) > 18}` );
+//console.log(`I can drive car: ${if(2021 - 1980) > 18){true}}` ); NOTE: This is not correct as if is statement and not expresion
+
