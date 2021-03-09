c4570a701ad1d5854b482871a5bc5adc4f0c7031
Rajeev Mishra <20rajeevmishra@gmail.com>
2021-03-09 16:50:13 +0100

    Basic Array Operations

--- 02-Fundamentals-Part-2/Basic Arrays Operations/script.js
+++ 02-Fundamentals-Part-2/Basic Arrays Operations/script.js
@@ -1,8 +1,31 @@
-//02-Fundamentals-Part-2 : Intro to Array 
+//02-Fundamentals-Part-2 : Basic Array Operations
 'use strict'; // Should be in begining.
 
 const friends = [`Micheal` , `Don` , `Max`]; // Literal format
 console.log(friends);
-friends.push(`Sam`);
+
+friends.push(`Sam`); // Add element to the end of the array - Return the length of the array
 console.log(friends);
 
+friends.unshift(`Tom`);// Add element to the start of the array - - Return the length of the array
+console.log(friends);
+
+
+const popped = friends.pop();// Removes the last element from the array - returns the removed element
+console.log(popped);
+console.log(friends); 
+
+
+const shifted = friends.shift(); // Removes the first element from the array - returns the removed element
+console.log(shifted);
+console.log(friends); 
+
+console.log(friends.indexOf(`Max`));
+console.log(friends.indexOf(`Maxq`)); // for element that does not exist it return -1
+
+console.log(friends.includes(`Max`)); // Strict equality check
+console.log(friends.includes(`Maxq`));
+
+friends.push(23);
+console.log(friends.includes(`23`));
+console.log (23 == `23`);
