f5dd7c304a0f9f231d35480daa881cf52af44e22
mish.ra.jeev <20rajeevmishra@gmail.com>
2021-03-09 12:22:57 +0100

    Strict Mode

---
+++ 02-Fundamentals-Part-2/Strict Mode/script.js
@@ -0,0 +1,12 @@
+'use strict'; // Should be in begining.
+
+let hasDriversLicense = false;
+const passTest = true;
+
+if(passTest){
+    hasDriversLicense = true;
+}
+if(hasDriversLicense){
+    console.log(`I can drive`);
+}
+
