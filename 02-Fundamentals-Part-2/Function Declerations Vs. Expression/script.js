b9103a7e38ad80ae58482681f33112bc8dc3230e
mish.ra.jeev <20rajeevmishra@gmail.com>
2021-03-09 12:57:21 +0100

    Function Declerations Vs. Expression

---
+++ 02-Fundamentals-Part-2/Function Declerations Vs. Expression/script.js
@@ -0,0 +1,29 @@
+//02-Fundamentals-Part-2 : Function Declerations Vs. Expression
+'use strict'; // Should be in begining.
+
+const currentYear = new Date().getFullYear();
+
+// Function Decleration
+// Can be called anywhere in the code. i.e. before or after decleration
+function calcAge1(birthYear) {
+    return currentYear - birthYear;
+}
+
+const age1Is = `Age is ${calcAge1(1990)}`;
+console.log(age1Is);
+
+
+// Function Expression
+// Can only be called after decleration
+/* const age2Is = `Age is ${calcAge2(1990)}`;
+console.log(age2Is); */ // this is throw error.
+const calcAge2 = function (birthYear) {
+    return currentYear - birthYear;
+}
+
+const age2Is = `Age is ${calcAge2(1990)}`;
+console.log(age2Is);
+
+
+
+console.log(age1Is, `-----` ,age2Is);
