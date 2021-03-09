c41ac9c1cbf673b0dc5d2ba7ce01b08bbb971ef3
mish.ra.jeev <20rajeevmishra@gmail.com>
2021-03-09 16:08:57 +0100

    Basic Array Operations

---
+++ 02-Fundamentals-Part-2/Basic Arrays Operations/script.js
@@ -0,0 +1,8 @@
+//02-Fundamentals-Part-2 : Intro to Array 
+'use strict'; // Should be in begining.
+
+const friends = [`Micheal` , `Don` , `Max`]; // Literal format
+console.log(friends);
+friends.push(`Sam`);
+console.log(friends);
+
