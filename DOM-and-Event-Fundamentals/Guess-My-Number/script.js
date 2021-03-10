ed44c72a099aef1a396a7c00ad67b27778cf4235
mish.ra.jeev <20rajeevmishra@gmail.com>
2021-03-10 10:02:47 +0100

    Selecting and Manipulating Elements

--- DOM-and-Event-Fundamentals/Guess-My-Number/script.js
+++ DOM-and-Event-Fundamentals/Guess-My-Number/script.js
@@ -4,6 +4,14 @@
     DOM is part of Web API
  */
 
-let ele = document.querySelector(`.message`)
-console.log(ele);
-console.log(ele.textContent);
+document.querySelector(`.message`);
+console.log(document.querySelector(`.message`));
+console.log(document.querySelector(`.message`).textContent);
+//ele.textContent = '';
+console.log(document.querySelector(`.number`));
+console.log(document.querySelector(`.score`));
+document.querySelector(`.number`).textContent = 13;
+document.querySelector(`.score`).textContent = 10;
+document.querySelector(`.guess`).value = 10;
+
+
