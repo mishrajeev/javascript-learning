14480450c478f950c2058ed2aee1f0ea6efffdff
mish.ra.jeev <20rajeevmishra@gmail.com>
2021-03-10 09:50:03 +0100

    DOM and DOM Manipulation Intro

--- DOM-and-Event-Fundamentals/Guess-My-Number/script.js
+++ DOM-and-Event-Fundamentals/Guess-My-Number/script.js
@@ -1,5 +1,9 @@
 'use strict';
 
+/*  DOM : Document Object Model : Structured representation of HTML Document
+    DOM is part of Web API
+ */
+
 let ele = document.querySelector(`.message`)
 console.log(ele);
 console.log(ele.textContent);
