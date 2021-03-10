d5cae03562e08ba08fc1bce43df16dd4fc6fb359
mish.ra.jeev <20rajeevmishra@gmail.com>
2021-03-10 14:20:53 +0100

    Modal Window - Intro and working with classes

---
+++ DOM-and-Event-Fundamentals/Modal-Window/script.js
@@ -0,0 +1,21 @@
+'use strict';
+const buttonElement = document.querySelectorAll(`.show-modal`);
+const modal = document.querySelector(`.modal`);
+const overlay = document.querySelector(`.overlay`);
+const closeModalButtom = document.querySelector(`.close-modal`);
+
+const closeModal = function (){
+    modal.classList.add(`hidden`);
+    overlay.classList.add(`hidden`);
+}
+
+const showModal = function (){
+    modal.classList.remove(`hidden`);
+    overlay.classList.remove(`hidden`);
+}
+
+for (let i = 0; i < buttonElement.length; i++) {
+    buttonElement[i].addEventListener(`click`, showModal);
+}
+closeModalButtom.addEventListener(`click`, closeModal);
+overlay.addEventListener(`click`, closeModal);
